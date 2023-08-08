4b07f76b6ed5439da9ac586a246c5f0b5ff2d6ad
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-08-08 23:44:50 +0800

    feat: Add done value in todoItem

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>
-    <h1>{{ props.item }}</h1>
+  <div class="todo-item">
+    <h3>{{ props.todoItem.todoText }}</h3>
+    <input type="checkbox" :checked="props.todoItem.done" />
   </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
-  item: string;
+  todoItem: {
+    todoText: string;
+    done: boolean;
+  };
 }>();
 </script>
+
+<style>
+.todo-item {
+  background: rgba(185, 156, 156, 0.185);
+  width: 400px;
+  margin: 10px 0;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home">
     <TodoInput @addItem="addItem" />
-    <TodoItem
-      v-for="(todoItem, index) in todoItems"
-      :key="index"
-      :item="todoItem"
-    />
+    <div class="todo-item-container">
+      <TodoItem
+        v-for="(todoItem, index) in todoItems"
+        :key="index"
+        :todoItem="todoItem"
+      />
+    </div>
   </div>
 </template>
 
@@ -14,9 +16,17 @@ import { ref } from "vue";
 import TodoInput from "@/components/TodoInput.vue";
 import TodoItem from "@/components/TodoItem.vue";
 
-const todoItems = ref<string[]>([]);
+const todoItems = ref<Array<{ todoText: string; done: boolean }>>([]);
 
 const addItem = (newItem: string) => {
-  todoItems.value.push(newItem);
+  todoItems.value.push({ todoText: newItem, done: false });
 };
 </script>
+
+<style>
+.todo-item-container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
