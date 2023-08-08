f4cd4894e1c64a1a9ca6c6447ee5132292ce54f3
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-08-08 21:28:47 +0800

    feat: Use event for addItem function

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,6 +1,8 @@
 <template>
-  <input v-model="newItem" type="text" />
-  <button @click="addItem(newItem)">Add</button>
+  <form @submit.prevent="addItem">
+    <input v-model="newItem" type="text" />
+    <button>Add</button>
+  </form>
   <div v-for="item in items" :key="item">
     <h2>{{ item }}</h2>
   </div>
@@ -12,8 +14,10 @@ import { ref } from "vue";
 const newItem = ref<string>("");
 const items = ref<string[]>([]);
 
-const addItem = (newItemValue: string) => {
-  items.value.push(newItemValue);
+const emit = defineEmits(["addItem"]);
+
+const addItem = () => {
+  emit("addItem", newItem.value);
   newItem.value = "";
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="home">
-    <TodoInput />
+    <TodoInput @addItem="addItem" />
+    <div v-for="item in todoItems" :key="item">
+      <h1>{{ item }}</h1>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import TodoInput from "@/components/TodoInput.vue";
+
+const todoItems = ref<string[]>([]);
+
+const addItem = (newItem: string) => {
+  todoItems.value.push(newItem);
+};
 </script>
