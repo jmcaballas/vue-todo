b1d5714411b6ee208a9f0040c4bf59bc494b12b1
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-08-08 20:12:44 +0800

    feat: Implement addITem function

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,4 +1,19 @@
 <template>
-  <input type="text" />
-  <button>Add</button>
+  <input v-model="newItem" type="text" />
+  <button @click="addItem(newItem)">Add</button>
+  <div v-for="item in items" :key="item">
+    <h2>{{ item }}</h2>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const newItem = ref(null);
+const items = ref([]);
+
+const addItem = () => {
+  items.value.push(newItem.value);
+  newItem.value = "";
+};
+</script>
